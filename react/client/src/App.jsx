b4ed0e3b0cd364149424f03b174b44b20e56011c
uttej528 <96988310+uttej528@users.noreply.github.com>
2024-10-01 00:55:54 +0530

    Add files via upload

---
+++ react/client/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Service from "./pages/Service";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ react/client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+
+
+const Login = () => {
+    return (
+      <div>
+        <h1>This is Login</h1>
+      </div>
+    )
+  }
+  
+  export default Login
+  
